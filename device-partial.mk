94d63985531cb4e30ae412e184092d3e2db468a6
Francesco Colella <mnemonyc@hotmail.com>
2014-06-09 14:56:34 +0200

    Update all Hazy apps

--- device-partial.mk
+++ device-partial.mk
@@ -6,14 +6,14 @@ PRODUCT_COPY_FILES += \
     vendor/twixkat/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon:google \
     vendor/twixkat/prebuilt/etc/install-recovery.sh:system/etc/install-recovery.sh:google \
     vendor/twixkat/proprietary/system/app/HazyApp.apk:system/app/HazyApp.apk:google \
-    vendor/twixkat/proprietary/system/app/TwixLight.apk:system/app/TwixLight.apk:google \
+    vendor/twixkat/proprietary/system/app/HazyLight.apk:system/app/HazyLight.apk:google \
     vendor/twixkat/proprietary/system/app/HazyWalls.apk:system/app/HazyWalls.apk:google \
     vendor/twixkat/proprietary/system/app/HazyVoice.apk:system/app/HazyVoice.apk:google \
 
 PRODUCT_PACKAGES += \
 	Superuser \
     HazyApp \
-    TwixLight \
+    HazyLight \
     HazyWalls \
     HazyVoice \
 
