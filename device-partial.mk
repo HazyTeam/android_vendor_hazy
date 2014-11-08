b563ea2fa29f70b015e9127ebe88de67c96c41f0
Francesco Colella <mnemonyc@hotmail.com>
2014-11-08 22:15:03 +0100

    Update SuperSU and add HazyCenter for Lollipop

--- device-partial.mk
+++ device-partial.mk
@@ -3,18 +3,19 @@ PRODUCT_COPY_FILES += \
     vendor/hazy/prebuilt/apk/Superuser.apk:system/app/Superuser.apk:google \
     vendor/hazy/prebuilt/bin/su:system/xbin/daemonsu:google \
     vendor/hazy/prebuilt/bin/su:system/xbin/su:google \
+    vendor/hazy/prebuilt/bin/supolicy:system/xbin/supolicy:google \
     vendor/hazy/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon:google \
     vendor/hazy/prebuilt/etc/install-recovery.sh:system/etc/install-recovery.sh:google \
-    vendor/hazy/proprietary/system/app/HazyCenter.apk:system/app/HazyCenter.apk:google \
-    vendor/hazy/proprietary/system/app/HazyLight.apk:system/app/HazyLight.apk:google \
+    vendor/hazy/proprietary/system/app/HazyCenter_test.apk:system/app/HazyCenter_test.apk:google \
+#    vendor/hazy/proprietary/system/app/HazyLight.apk:system/app/HazyLight.apk:google \
     vendor/hazy/proprietary/system/app/HazyWalls.apk:system/app/HazyWalls.apk:google \
-    vendor/hazy/proprietary/system/app/HazyVoice.apk:system/app/HazyVoice.apk:google \
+#    vendor/hazy/proprietary/system/app/HazyVoice.apk:system/app/HazyVoice.apk:google \
 
 PRODUCT_PACKAGES += \
 	Superuser \
-    HazyCenter \
-    HazyLight \
+    HazyCenter_test \
+#    HazyLight \
     HazyWalls \
-    HazyVoice \
+#    HazyVoice \
 
 $(call inherit-product, vendor/hazy/configs/aosp_fixes.mk)
