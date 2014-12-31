3e9d48de559d5df62a1029782d7fc53d4ef0103c
Francesco Colella <mnemonyc@hotmail.com>
2014-12-31 17:24:49 +0100

    Update HazyCenter and fix common.mk

--- configs/common.mk
+++ configs/common.mk
@@ -100,10 +100,10 @@ PRODUCT_COPY_FILES += \
 
 # Add Hazy packages
 PRODUCT_COPY_FILES += \
-    vendor/hazy/proprietary/system/app/HazyCenter_test.apk:system/app/HazyCenter.apk \
+    vendor/hazy/proprietary/system/app/HazyCenter.apk:system/app/HazyCenter.apk \
     vendor/hazy/proprietary/system/app/HazyWalls.apk:system/app/HazyWalls.apk \
-    vendor/hazy/proprietary/system/app/Superuser.apk:system/app/Superuser.apk \
-    vendor/hazy/proprietary/system/bin/su.apk:system/bin/su
+    vendor/hazy/prebuilt/system/apk/Superuser.apk:system/app/Superuser.apk \
+    vendor/hazy/prebuilt/system/bin/su.apk:system/bin/su
 
 # Versioning
 -include vendor/hazy/configs/version.mk
