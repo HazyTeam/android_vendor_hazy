ac4d797f1b8082c461ee2c9dc1734bff5f9dbd1b
Francesco Colella <mnemonyc@hotmail.com>
2015-01-16 19:03:47 +0100

    Add support for HTC One M8

--- configs/common.mk
+++ configs/common.mk
@@ -5,7 +5,7 @@ ifneq ($(filter hazy_i9300 hazy_mako hazy_maguro hazy_p3100 hazy_manta hazy_d802
     PRODUCT_COPY_FILES += \
         vendor/hazy/prebuilt/bootanimation/1280x720.zip:system/media/bootanimation.zip
 endif
-ifneq ($(filter hazy_hammerhead hazy_bacon hazy_falcon,$(TARGET_PRODUCT)),)
+ifneq ($(filter hazy_hammerhead hazy_bacon hazy_falcon hazy_m8,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/hazy/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
 endif
