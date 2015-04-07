1838896f1836f3cdeb0135c60c2aa50614051f13
ValoXTech <valoxblood@gmail.com>
2015-04-07 15:29:41 +0200

    EOS to Hazy (Thanks to Euphoria)
    Change-Id: Ibcdb891cf9b8aa05a150efba11fe88f2b71bd459

--- prebuilt/common/bin/50-hazy.sh
+++ prebuilt/common/bin/50-hazy.sh
@@ -1,8 +1,9 @@
 #!/sbin/sh
 #
-# /system/addon.d/50-eos.sh
+# /system/addon.d/50-hazy.sh
 # During a upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
+# Big thanks to Euphoria
 #
 
 . /tmp/backuptool.functions
