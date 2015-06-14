6b3be37be33f331ff8687822ae9129ca38147375
Francesco Colella <mnemonyc@hotmail.com>
2015-06-14 23:57:20 +0200

    Update ringtone and alarm sound

--- configs/common_full_phone.mk
+++ configs/common_full_phone.mk
@@ -1,9 +1,10 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/hazy/configs/common_full.mk)
-# Default notification/alarm sounds
+# Default notification/alarm sounds/ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.config.notification_sound=Argon.ogg \
-ro.config.alarm_alert=Hassium.ogg
+ro.config.alarm_alert=Argon.ogg \
+ro.config.ringtone=Sedna.ogg
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 PRODUCT_COPY_FILES += \
 vendor/hazy/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
