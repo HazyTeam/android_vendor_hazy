04338d4484e31af35cac4c83a9fae008db4eb99f
Francesco Colella <mnemonyc@hotmail.com>
2015-09-20 17:44:27 +0200

    Error commit

--- configs/gsm.mk
+++ configs/gsm.mk
@@ -1,6 +1,6 @@
 # GSM APN list
 PRODUCT_COPY_FILES += \
-    vendor/hazy/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/hazy/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
